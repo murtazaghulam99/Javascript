fde9c0568c9f5fbd7f5762f1a411910c24eaf06d
Ghulam Murtaza <76404809+murtazaghulam99@users.noreply.github.com>
2022-10-10 19:17:38 +0500

    Add files via upload

---
+++ eCommerce/Poll UI Design/script.js
@@ -0,0 +1,14 @@
+const options = document.querySelectorAll("label");
+for (let i = 0; i < options.length; i++) {
+    options[i].addEventListener("click", () => {
+        for (let k = 0; k < options.length; k++) {
+            if(options[k].classList.contains("selected")) {
+                options[k].classList.remove("selected");
+            }
+        }
+        options[i].classList.add("selected");
+        for (let j = 0; j < options.length; j++) {
+            options[j].classList.add("selectall");
+        }        
+    })
+}
