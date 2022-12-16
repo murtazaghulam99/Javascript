e275423321c893fa5df963a31dbe2283aed6ce05
Ghulam Murtaza <76404809+murtazaghulam99@users.noreply.github.com>
2022-12-17 02:36:45 +0500

    Delete DSA directory

--- DSA/Telephone Number Validator/index.js
+++
@@ -1,6 +0,0 @@
-function telephoneCheck(str) {
-    let regExp = /^(1\s?)?(\d{3}|\(\d{3}\))[\-\s]?\d{3}[\-\s]?\d{4}$/;
-    return regExp.test(str);
-  }
-  
-  console.log(telephoneCheck("555-555-5555"));
